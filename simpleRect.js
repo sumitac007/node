e7651273f3d9d3dbd1b8dd60b77d6ff5f550c75e
sumitac007 <sumitchinchane@gmail.com>
2016-09-17 17:49:03 +0530

    Simple Rect first module example

---
+++ simpleRect.js
@@ -0,0 +1,22 @@
+var rect = {
+	perimeter: function (x,y){
+		return (2*(x+y));
+	},
+	area: function (x,y){
+		return (x*y);
+	}  
+};
+function solveRect(l,b){
+
+	console.log("Solving Rectangle ...");
+
+	if(l<0 || b < 0){
+		console.log("Lenght or breadth is less than 0");
+	}
+	else {
+		console.log("Area of the rectange is  " + rect.area(l,b) + " and perimeter is " + rect.perimeter(l,b) );
+	}
+
+}	
+solveRect(1,2);
+solveRect(-1,2);
